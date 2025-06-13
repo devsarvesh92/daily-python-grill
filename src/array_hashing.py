586a4bedabb76073f0dc8246940d70de162286a6
sarvesh sawant <sarvesh@sarveshs-MacBook-Pro-2.local>
2025-06-13 17:39:41 +0530

    Solve product of array except self

--- src/array_hashing.py
+++ src/array_hashing.py
@@ -1,11 +1,7 @@
-
-
-
-
 from collections import defaultdict
 
 
-def group_anagrams(*,input:list[str])->list[list[str]]:
+def group_anagrams(*, input: list[str]) -> list[list[str]]:
     """
     Group anagrams
 
@@ -20,16 +16,29 @@ def group_anagrams(*,input:list[str])->list[list[str]]:
     for ip in input:
         gp["".join(sorted(ip))].append(ip)
 
-    
-    return [v for k,v in gp.items()]
+    return [v for k, v in gp.items()]
 
-def top_k_frequent_elements(*,nums:list[int],k:int)->list[int]:
-    d:dict[int,int] = defaultdict(int)
+
+def top_k_frequent_elements(*, nums: list[int], k: int) -> list[int]:
+    d: dict[int, int] = defaultdict(int)
 
     for i in nums:
-        d[i]+=1
-    
-    sd:dict[int,int] = dict(sorted(d.items(),key=lambda k:k[1],reverse=True))
+        d[i] += 1
+
+    sd: dict[int, int] = dict(sorted(d.items(), key=lambda k: k[1], reverse=True))
     return list(sd.values())[:k]
 
 
+def product_of_array_execept_self(*, nums: list[int]) -> list[int]:
+    result = [1] * len(nums)
+    product = 1
+    for id, num in enumerate(nums):
+        result[id] = product
+        product *= num
+
+    suffix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        result[i] *= suffix
+        suffix *= nums[i]
+
+    return result
--- tests/test_array_hashing.py
+++ tests/test_array_hashing.py
@@ -1,16 +1,24 @@
-
-
-
 import pytest
 
-from src.array_hashing import group_anagrams, top_k_frequent_elements
+from src.array_hashing import (
+    group_anagrams,
+    top_k_frequent_elements,
+    product_of_array_execept_self,
+)
 
 
 pytestmark = pytest.mark.array_hashing
 
 
 def test_group_anagram():
-    assert [['act', 'cat'], ['pots', 'tops', 'stop'], ['hat']] == group_anagrams(input=["act","pots","tops","cat","stop","hat"])
+    assert [["act", "cat"], ["pots", "tops", "stop"], ["hat"]] == group_anagrams(
+        input=["act", "pots", "tops", "cat", "stop", "hat"]
+    )
+
 
 def test_top_k_frequent_elements():
-    assert [3,2] == top_k_frequent_elements(k=2,nums=[1,2,2,3,3,3])
+    assert [3, 2] == top_k_frequent_elements(k=2, nums=[1, 2, 2, 3, 3, 3])
+
+
+def test_product_of_array_except_self():
+    assert [48, 24, 12, 8] == product_of_array_execept_self(nums=[1, 2, 4, 6])
